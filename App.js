ff91970a1e1c8b613ed19749b8238c237c2a88a2
4mshu <2hungrygators@gmail.com>
2024-08-12 11:28:18 +0545

    removed dist and parcel-cache

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div",
     {id: "parent"},
     [React.createElement("div", {id: "child"}, 
-        [React.createElement("h1", {}, "I am an h1 Tag"), React.createElement("h2", {}, "I am an h2 Tag")]),
+        [React.createElement("h1", {}, "this is sparta!!"), React.createElement("h2", {}, "I am an h2 Tag")]),
         React.createElement("div", {id: "child2"}, 
             [React.createElement("h1", {}, "I am an h1 Tag"), React.createElement("h2", {}, "I am an h2 Tag")])]
     )
